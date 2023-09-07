1553c2f465b0e3207389730d7d4ec9f299674c46
LMelloNia <ch10152080@gmail.com>
2023-09-07 15:23:13 +0900

    강의22
    SearchController

---
+++ InstagramClone/InstagramClone/Controller/ProfileController 2.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileController.swift
+//  InstagramClone
+//
+//  Created by 김시훈 on 2023/08/17.
+//
+
+import UIKit
+
+class ProfileController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        view.backgroundColor = .systemPurple
+    }
+}
