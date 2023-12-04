85e627f0a71a4f2b0c3e3d0d14a23160c3ab7e4c
LMelloNia <ch10152080@gmail.com>
2023-12-04 15:51:56 +0900

    강의 32

---
+++ InstagramClone/InstagramClone/API/ PostService.swift
@@ -0,0 +1,26 @@
+//
+//   PostService.swift
+//  InstagramClone
+//
+//  Created by 김시훈 on 12/4/23.
+//
+
+import UIKit
+import Firebase
+
+struct PostService {
+
+    static func uploadPost(caption: String, image: UIImage, completion: @escaping(FirestoreComplition)) {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+
+        ImageUploader.uploadImage(image: image) { imageUrl in
+            let data = ["caption": caption,
+                        "timestamp": Timestamp(date: Date()),
+                        "likes": 0,
+                        "imageUrl": imageUrl,
+                        "ownerUid": uid] as [String : Any]
+
+            COLLECTION_POSTS.addDocument(data: data, completion: completion)
+        }
+    }
+}
